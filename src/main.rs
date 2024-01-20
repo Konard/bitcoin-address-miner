d40e82c113e52ce8fab19f5d572010d71a1473cc
Konstantin Dyachenko <drakonard@gmail.com>
2024-01-20 14:53:49 +0000

    Update min_secret_key_bytes (30273000 private keys checked)

--- src/main.rs
+++ src/main.rs
@@ -97,7 +97,9 @@ async fn main() {
     let target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so";
     // let target_address = "1LgpDjsqkxF9cTkz3UYSbdTJuvbZ45PKvx";
     
-    let min_secret_key_bytes = decode_hex("0000000000000000000000000000000000000000000000020000000000000000").unwrap();
+    // let min_secret_key_bytes = decode_hex("0000000000000000000000000000000000000000000000020000000000000000").unwrap();
+    let min_secret_key_bytes = decode_hex("0000000000000000000000000000000000000000000000020000000001cdede8").unwrap();
+
     let max_secret_key_bytes = decode_hex("000000000000000000000000000000000000000000000003ffffffffffffffff").unwrap();
 
     // let min_secret_key_bytes = decode_hex("0000000000000000000000000000000000000000000000020000000000000000").unwrap();
