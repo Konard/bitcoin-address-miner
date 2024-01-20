cff80bd20abcc1a166fe42fa6b6742c1805c08df
Konstantin Dyachenko <drakonard@gmail.com>
2024-01-20 21:08:01 +0300

    Comment unused code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bitcoin::{Address, PublicKey, Network};
-use bitcoin::secp256k1::{rand, Secp256k1, SecretKey, PublicKey as SecpPublicKey, Signing};
-use hex::encode_upper;
+// use bitcoin::secp256k1::{rand, Secp256k1, SecretKey, PublicKey as SecpPublicKey, Signing};
+use bitcoin::secp256k1::{Secp256k1, SecretKey, PublicKey as SecpPublicKey, Signing};
+// use hex::encode_upper;
 use std::time::Instant;
 use std::{fmt::Write, num::ParseIntError};
 
@@ -19,49 +20,49 @@ pub fn encode_hex(bytes: &[u8]) -> String {
     s
 }
 
-fn check_allowed_chars(starting_letters: &str) -> bool {
-    let allowed_chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-    starting_letters.chars().all(|c| allowed_chars.contains(c))
-}
+// fn check_allowed_chars(starting_letters: &str) -> bool {
+//     let allowed_chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+//     starting_letters.chars().all(|c| allowed_chars.contains(c))
+// }
 
-async fn mine_address(starting_letters: String) {
-    if !check_allowed_chars(&starting_letters) {
-        println!("Allowed chars: 123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz");
-        std::process::exit(1);
-    }
+// async fn mine_address(starting_letters: String) {
+//     if !check_allowed_chars(&starting_letters) {
+//         println!("Allowed chars: 123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz");
+//         std::process::exit(1);
+//     }
 
-    let secp = Secp256k1::new();
+//     let secp = Secp256k1::new();
 
-    // Initialize an Instant to measure elapsed time.
-    let start_time = Instant::now();
+//     // Initialize an Instant to measure elapsed time.
+//     let start_time = Instant::now();
 
-    loop {
-        // Generate random key pair.
-        let (secret_key, public_key) = secp.generate_keypair(&mut rand::thread_rng());
+//     loop {
+//         // Generate random key pair.
+//         let (secret_key, public_key) = secp.generate_keypair(&mut rand::thread_rng());
 
-        // Convert secp256k1::PublicKey to bitcoin::PublicKey.
-        let bitcoin_public_key =
-            PublicKey::from(SecpPublicKey::from_slice(&public_key.serialize()[..]).unwrap());
+//         // Convert secp256k1::PublicKey to bitcoin::PublicKey.
+//         let bitcoin_public_key =
+//             PublicKey::from(SecpPublicKey::from_slice(&public_key.serialize()[..]).unwrap());
 
-        // Generate pay-to-pubkey-hash address.
-        let address = Address::p2pkh(&bitcoin_public_key, Network::Bitcoin);
+//         // Generate pay-to-pubkey-hash address.
+//         let address = Address::p2pkh(&bitcoin_public_key, Network::Bitcoin);
 
-        // Ignore the specified number of characters when checking for a match.
-        let address_tail = &address.to_string()[1..]; // Start from the second character onward
+//         // Ignore the specified number of characters when checking for a match.
+//         let address_tail = &address.to_string()[1..]; // Start from the second character onward
 
-        // Uncomment the following line for debug: print the current address being attempted.
-        //println!("Trying address: {}", address);
+//         // Uncomment the following line for debug: print the current address being attempted.
+//         //println!("Trying address: {}", address);
 
-        // Check if the address (excluding the specified characters) starts with the desired letters.
-        if address_tail.starts_with(&starting_letters) {
-            println!("Found matching address: {}", address);
-            println!("Private Key: {}", encode_upper(secret_key.as_ref()));
-            println!("Public Key: {}", bitcoin_public_key);
-            println!("Elapsed Time: {:.2} minutes", start_time.elapsed().as_secs_f64() / 60.0);
-            std::process::exit(0);
-        }
-    }
-}
+//         // Check if the address (excluding the specified characters) starts with the desired letters.
+//         if address_tail.starts_with(&starting_letters) {
+//             println!("Found matching address: {}", address);
+//             println!("Private Key: {}", encode_upper(secret_key.as_ref()));
+//             println!("Public Key: {}", bitcoin_public_key);
+//             println!("Elapsed Time: {:.2} minutes", start_time.elapsed().as_secs_f64() / 60.0);
+//             std::process::exit(0);
+//         }
+//     }
+// }
 
 fn increment_bytes(b256: &mut [u8], mut amount: u64) -> u64 {
     let mut i = b256.len() - 1;
@@ -118,6 +119,7 @@ async fn main() {
     let mut current_secret_key_bytes = min_secret_key_bytes.to_vec();
 
     let mut i = 0;
+    let mut before = Instant::now();
 
     while current_secret_key_bytes != max_secret_key_bytes {
         let address = private_key_to_address(&secp, &current_secret_key_bytes);
@@ -128,8 +130,10 @@ async fn main() {
             break;
         }
 
-        if i % 1_000 == 0 {
+        if i % 100_000 == 0 {
             println!("current secret key: {}", encode_hex(&current_secret_key_bytes));
+            println!("elapsed time: {:.2?}", before.elapsed());
+            before = Instant::now();
         }
 
         i = i + 1;
@@ -141,45 +145,45 @@ async fn main() {
 
     return;
 
-    increment_bytes(&mut secret_key_bytes, 1);
+    // increment_bytes(&mut secret_key_bytes, 1);
 
-    let secret_key = SecretKey::from_slice(&secret_key_bytes).unwrap();
+    // let secret_key = SecretKey::from_slice(&secret_key_bytes).unwrap();
 
-    let public_key = SecpPublicKey::from_secret_key(&secp, &secret_key);
+    // let public_key = SecpPublicKey::from_secret_key(&secp, &secret_key);
 
-    let bitcoin_public_key =
-    PublicKey::from(SecpPublicKey::from_slice(&public_key.serialize()[..]).unwrap());
+    // let bitcoin_public_key =
+    // PublicKey::from(SecpPublicKey::from_slice(&public_key.serialize()[..]).unwrap());
 
-    let address = Address::p2pkh(&bitcoin_public_key, Network::Bitcoin);
+    // let address = Address::p2pkh(&bitcoin_public_key, Network::Bitcoin);
 
-    println!("secret_key: {}", secret_key.display_secret());
-    // println!("{}", public_key);
-    println!("bitcoin_address: {}", address);
+    // println!("secret_key: {}", secret_key.display_secret());
+    // // println!("{}", public_key);
+    // println!("bitcoin_address: {}", address);
 
-    return;
-    // Ask the user for the starting letters.
-    println!("[BITCOIN KEY/ADDRESS MINING] \n Enter the starting letters for the address: ");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Failed to read input");
-    let starting_letters = input.trim().to_string();
-    println!("\n Starting mining...this may take many minutes (or even days) to complete");
+    // return;
+    // // Ask the user for the starting letters.
+    // println!("[BITCOIN KEY/ADDRESS MINING] \n Enter the starting letters for the address: ");
+    // let mut input = String::new();
+    // std::io::stdin().read_line(&mut input).expect("Failed to read input");
+    // let starting_letters = input.trim().to_string();
+    // println!("\n Starting mining...this may take many minutes (or even days) to complete");
 
-    // Check if all characters in starting_letters are allowed.
-    if !check_allowed_chars(&starting_letters) {
-        println!("You entered a wrong char - allowed chars: 123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz");
-        std::process::exit(1);
-    }
+    // // Check if all characters in starting_letters are allowed.
+    // if !check_allowed_chars(&starting_letters) {
+    //     println!("You entered a wrong char - allowed chars: 123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz");
+    //     std::process::exit(1);
+    // }
 
-    // Run multiple async threads to mine addresses concurrently.
-    let mut handles = vec![];
-    // 50 parallel threads
-    for _ in 0..50 {
-        let handle = tokio::spawn(mine_address(starting_letters.clone()));
-        handles.push(handle);
-    }
+    // // Run multiple async threads to mine addresses concurrently.
+    // let mut handles = vec![];
+    // // 50 parallel threads
+    // for _ in 0..50 {
+    //     let handle = tokio::spawn(mine_address(starting_letters.clone()));
+    //     handles.push(handle);
+    // }
 
-    // Wait for all threads to complete.
-    for handle in handles {
-        handle.await.expect("Error in thread");
-    }
+    // // Wait for all threads to complete.
+    // for handle in handles {
+    //     handle.await.expect("Error in thread");
+    // }
 }
