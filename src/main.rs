c71e31affaaf1247933a952a018f2f3f44c064ef
Konstantin Dyachenko <drakonard@gmail.com>
2024-01-21 13:51:47 +0300

    Add draft of math replication using rug

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use bitcoin::{Address, PublicKey, Network};
 // use bitcoin::secp256k1::{rand, Secp256k1, SecretKey, PublicKey as SecpPublicKey, Signing};
 use bitcoin::secp256k1::{Secp256k1, SecretKey, PublicKey as SecpPublicKey, Signing};
+use std::fmt::Debug;
 // use hex::encode_upper;
 use std::time::{Instant, Duration};
 use std::{fmt::Write, num::ParseIntError};
+use rug::{Assign, Integer, Complete};
 
 pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
     (0..s.len())
@@ -79,8 +81,21 @@ fn increment_bytes(b256: &mut [u8], mut amount: u64) -> u64 {
     amount
 }
 
+fn append_to_32(bytes: &[u8]) -> Vec<u8> {
+    if bytes.len() < 32 {
+        let mut result: Vec<u8> = vec![0; 32 - bytes.len()];
+        result.extend_from_slice(bytes);
+        return result;
+    } else {
+        let mut result: Vec<u8> = vec![];
+        result.extend_from_slice(bytes);
+        return result;
+    }
+}
+
 fn private_key_to_address<C: Signing>(secp: &Secp256k1<C>, bytes: &[u8]) -> bitcoin::Address {
-    let secret_key = SecretKey::from_slice(&bytes).unwrap();
+    let processed_bytes = append_to_32(bytes);
+    let secret_key = SecretKey::from_slice(&processed_bytes).unwrap();
     let public_key = SecpPublicKey::from_secret_key(&secp, &secret_key);
     let secp_public_key = SecpPublicKey::from_slice(&public_key.serialize()[..]).unwrap();
     let bitcoin_public_key = PublicKey::from(secp_public_key);
@@ -91,6 +106,9 @@ fn private_key_to_address<C: Signing>(secp: &Secp256k1<C>, bytes: &[u8]) -> bitc
 
 #[tokio::main]
 async fn main() {
+
+    // let mut int = Integer::new();
+    // int.assign(14);
 
     let secp = Secp256k1::new();
 
@@ -112,8 +130,33 @@ async fn main() {
     // let min_secret_key_bytes = decode_hex("00000000000000000000000000000000000000000000000200000000037d5b38").unwrap();
     // let min_secret_key_bytes = decode_hex("0000000000000000000000000000000000000000000000020000000011127bc0").unwrap();
     // let min_secret_key_bytes = decode_hex("000000000000000000000000000000000000000000000002000000001314b460").unwrap();
-    let min_secret_key_bytes = decode_hex("000000000000000000000000000000000000000000000002000000007cf7f180").unwrap();
+    // let min_secret_key_bytes = decode_hex("000000000000000000000000000000000000000000000002000000007cf7f180").unwrap();
+    let min_secret_key_bytes = decode_hex("000000000000000000000000000000000000000000000002000000008f847a80").unwrap();
     let max_secret_key_bytes = decode_hex("000000000000000000000000000000000000000000000003ffffffffffffffff").unwrap();
+
+    let int = Integer::parse_radix("000000000000000000000000000000000000000000000002000000007cf7f180", 16).unwrap();
+    println!("{:?}", int);
+    let int2 = Integer::parse_radix("000000000000000000000000000000000000000000000003ffffffffffffffff", 16).unwrap();
+    // println!("{:?}", int.complete() < int2.complete());
+    // println!("{}", int.complete().to_string_radix(16));
+    // println!("{}", int2.complete().to_string_radix(16));
+
+    let min_bytes = int.complete().to_digits::<u8>(rug::integer::Order::MsfBe);
+    let max_bytes = int2.complete().to_digits::<u8>(rug::integer::Order::MsfBe);
+
+    println!("{:?}", min_bytes);
+    println!("{:?}", max_bytes);
+
+    println!("{:?}", min_secret_key_bytes);
+    println!("{:?}", max_secret_key_bytes);
+
+    // let mut vec1: Vec<u8> = vec![0; 23];
+    // vec1.append(&mut min_bytes);
+
+    let address1 = private_key_to_address(&secp, &min_secret_key_bytes);
+    let address2 = private_key_to_address(&secp, &min_bytes);
+    println!("address1: {}", address1);
+    println!("address2: {}", address2);
 
     // let min_secret_key_bytes = decode_hex("0000000000000000000000000000000000000000000000020000000000000000").unwrap();
     // let max_secret_key_bytes = decode_hex("0000000000000000000000000000000000000000000000020000000000000002").unwrap();
